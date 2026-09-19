97a14befd63ae62ac8531052da8d7557826c8115
Ahmed Fathy <ahmedfathi20044002@gmail.com>
2026-09-19 14:04:03 +0300

    style(awards): dramatically increase trophy size and downward translation

--- src/features/awards/components/TrophyRow.tsx
+++ src/features/awards/components/TrophyRow.tsx
@@ -97,7 +97,7 @@ export const TrophyRow = ({
             <img
               src={award.image_url || trophy}
               alt={award.title}
-              className="w-24 h-32 sm:w-32 sm:h-40 md:w-36 md:h-44 lg:w-40 lg:h-48 object-contain scale-[1.3] sm:scale-[1.4] origin-bottom translate-y-4 sm:translate-y-6 md:translate-y-7"
+              className="w-24 h-32 sm:w-32 sm:h-40 md:w-36 md:h-44 lg:w-40 lg:h-48 object-contain scale-[1.6] sm:scale-[1.7] md:scale-[1.8] origin-bottom translate-y-8 sm:translate-y-10 md:translate-y-12"
             />
             {award.won_count > 0 ? (
               <div
